57c2a7b28ac9e6389e03e2e8266d11ffe2dae494
Gokulrajv1304 <gvenugo1@my.centennialcollege.ca>
2022-02-04 03:43:23 -0500

    First Version of Assignment1

---
+++ Client/Scripts/app.js
@@ -0,0 +1,15 @@
+/*File Name: GokulrajVenugopal_COMP229_Assignement1
+Student Name: Gokulraj Venugopal
+Student ID: 301202722
+Date: 04-Feb-2022 
+*/
+
+//IIFE -- Immediately Invoked Function Expression
+
+"use strict";
+(function () {
+    function Start() {
+        console.log("App Started");
+    }
+    window.addEventListener("load", Start);
+})();
---
+++ Server/Routes/index.js
@@ -0,0 +1,32 @@
+/*File Name: GokulrajVenugopal_COMP229_Assignement1
+Student Name: Gokulraj Venugopal
+Student ID: 301202722
+Date: 04-Feb-2022 
+*/
+
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const router = express_1.default.Router();
+exports.default = router;
+router.get('/', function (req, res, next) {
+    res.render('index', { title: 'Home', page: 'home' });
+});
+router.get('/home', function (req, res, next) {
+    res.render('index', { title: 'Home', page: 'home' });
+});
+router.get('/about', function (req, res, next) {
+    res.render('index', { title: 'About Us', page: 'about' });
+});
+router.get('/projects', function (req, res, next) {
+    res.render('index', { title: 'Our Projects', page: 'projects' });
+});
+router.get('/services', function (req, res, next) {
+    res.render('index', { title: 'Our Services', page: 'services' });
+});
+router.get('/contact', function (req, res, next) {
+    res.render('index', { title: 'Contact Us', page: 'contact' });
+});
